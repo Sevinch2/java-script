9fdf877f3c03fcc80ce90041014e8a1a7ff53d00
ali <bovaqulov@gmail.com>
2024-03-09 12:03:32 +0500

    java script

---
+++ script.js
@@ -0,0 +1,12 @@
+var yosh = +prompt("Yoshingizni kiriting")
+if (yosh>=18) {
+    alert("Yoshsiz. O'qishingiz kerak")
+}else if (yosh>=50){
+    alert("Ishlashingiz kerak")
+}else if (yosh>=59){
+    alert("Yaqinda pensiyaga chiqasiz")
+}else if (yosh>=150){
+    alert("Pensionerga o'xshaysiz, hali tirik bo'lsangiz")
+}else{
+    alert("Noto'g'ri")
+}
